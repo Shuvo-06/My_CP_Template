7ba154e2da351fefcb46905a2f7d4568ec171229
Shuvo-06 <168964479+Shuvo-06@users.noreply.github.com>
2024-07-17 00:40:13 +0600

    Create opening_template.cpp

---
+++ opening_template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define nl << "\n";
+#define ll long long int
+#define forl(i, st, en) for (int i = st; i < en; i++)
+#define testcase(t) int t; cin >> t; while (t--)
+#define db(x) cout << #x << "->" << x nl
+#define view(v) forl(abac, 0, v.size()) cout << v[abac] << " "; cout nl
+#define eb emplace_back()
+#define prec(abaca) fixed<<setprecision(abaca)
+#define rounded(a,b) ((a+(b/2))/b)
+#define ceiled(a,b) ((a+b-1)/b)
+#define YES cout << "YES" nl
+#define NO cout << "NO" nl
+
+int main() {
+  fast;
+  //testcase(tt) solve();
+  //solve();
+  return 0;
+}
