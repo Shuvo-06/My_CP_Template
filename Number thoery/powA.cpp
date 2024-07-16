25375b5e3313b3d81210ad26ae33ce36a324fbb9
Shuvo-06 <168964479+Shuvo-06@users.noreply.github.com>
2024-07-16 14:04:52 +0600

    Create powA.cpp

---
+++ Number thoery/powA.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long int powA(long long int a, long long int b, long long int mod) {
+    long long int ans=1;
+    while (b) {
+        if (b&1) ans= (ans*a)%mod;
+        a = (a*a)%mod;
+        b >>= 1;
+    }
+    return ans%mod ;
+}
+
+int main(){
+  //source : Luv(Youtube channel name : Luv)
+  //use this when a is very big in (a^b)%mod or the base case
+  //time complexity log2 (b)
+  return 0;
+}
