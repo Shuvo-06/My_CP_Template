534542a99c5b5dcf9b2b9d1bd885e1dd2ccd39a7
Shuvo-06 <168964479+Shuvo-06@users.noreply.github.com>
2024-12-17 22:25:31 +0600

    Update powA.cpp

--- Number thoery/powA.cpp
+++ Number thoery/powA.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-long long int powA(long long int a, long long int b, long long int mod) {
+long long int powA(long long int a, long long int b, long long int mod = 1000000007) {
     long long int ans=1;
     while (b) {
         if (b&1) ans= (ans*a)%mod;
