7a7683c5cad12d850f53aed7d170b1d1e2bd1951
Shuvo-06 <168964479+Shuvo-06@users.noreply.github.com>
2025-06-01 23:10:15 +0600

    Update Prefix_Sum_2D.cpp

--- Miscellaneous/Prefix_Sum_2D.cpp
+++ Miscellaneous/Prefix_Sum_2D.cpp
@@ -1,32 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Prefix2D {
+private:
+    int n, m;
+    vector <vector<long long int>> prefix;
+
+public:
+    Prefix2D(vector< vector <long long int>> &grid) {
+        n = grid.size();
+        m = grid[0].size();
+        prefix.assign(n + 1, std::vector<long long int>(m + 1, 0));
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                prefix[i][j] = grid[i - 1][j - 1] + prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1];
+            }
+        }
+    }
+
+    // sum from top left (x1, y1) to bottom right (x2, y2) // 0 - based
+    long long int query(int x1, int y1, int x2, int y2) {
+        x1++, y1++, x2++, y2++;
+        return prefix[x2][y2] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1];
+    }
+};
 
 int main() {
-  //used to calculate prefix sum on 2D array
-  //time complexity -> O(n * m + q) 
-  //top left corner -> (x1, y1)
-  //bottom right corner -> (x2, y2)
+  // used to calculate prefix sum on 2D array
+  // time complexity -> O(n * m + q) 
   
   int n,m;
   cin >> n >> m;
-  vector <vector<int>> pref(n + 1, vector<int>(m + 1, 0)), a(n, vector<int>(m));
+  vector <vector<long long int>> a(n, vector<long long int>(m));
   
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= m; j++) cin >> a[i][j];
   }
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= m; j++) {
-      pref[i][j] = pref[i - 1][j] + pref[i][j - 1] - pref[i - 1][j - 1] + a[i][j];
-    }
-  }
+
+  Prefix2D pref(a);
   
   int q;
   cin >> q;
   while (q--){
     int x1, y1, x2, y2;
     cin >> r1 >> c1 >> r2 >> c2;
-    cout << pref[x2][y2] - pref[x1 - 1][y2] - pref[x2][y1 - 1] + pref[x1 - 1][y1 - 1] << '\n';
+    cout << pref.query(x1, y1, x2, y2) << "\n";
   }
   return 0;
 }
