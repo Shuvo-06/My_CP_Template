018c47f614e0e2a917203ec92777d8b30b68fef8
Shuvo-06 <168964479+Shuvo-06@users.noreply.github.com>
2025-06-06 18:52:49 +0600

    Update Fibonacci_with_Query.cpp

--- Number thoery/Fibonacci_with_Query.cpp
+++ Number thoery/Fibonacci_with_Query.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 const long long int MOD = 1000000007;
 
+/*
 // iterative version
 // logN per query
 // can't support heavy querying, eg query count >= 1e7 for dp or other stuffs
@@ -35,4 +36,30 @@ int main(){
 	  	cout << fib(x, 1, 1) << "\n";
 	}
 	return 0;
+}
+*/
+
+const long long int MOD = 1e9 + 7;
+ 
+map <long long int, pair <long long int, long long int> > fib_memo;
+pair <long long int, long long int> fib_internal (long long int n) {
+    if (fib_memo.count(n)) return fib_memo[n];
+ 
+    auto [ft1, ft2] = fib_internal(n >> 1);
+    long long int c = (ft1 * ((2 * ft2) % MOD - ft1 + MOD) % MOD) % MOD;
+    long long int d = ((ft1 * ft1) % MOD + (ft2 * ft2) % MOD) % MOD;
+    
+    if (n & 1) return fib_memo[n] = {d, (c + d) % MOD};
+    else return fib_memo[n] = {c, d};
+}
+long long int fib(long long int n) {return fib_internal(n).first;}
+ 
+int32_t main() {
+    fib_memo[0] = {0, 1}; // first and second element
+	// time complexity : O(logn * loglogn) 
+	// allows heavy querying due to memoization
+    
+    long long int n;
+    cin >> n;
+    cout << fib(n);
 }
