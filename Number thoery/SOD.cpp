71601842ac0c935251e99d84ddc24c2ffff2f880
Shuvo-06 <168964479+Shuvo-06@users.noreply.github.com>
2025-06-06 22:30:53 +0600

    Update SOD.cpp

--- Number thoery/SOD.cpp
+++ Number thoery/SOD.cpp
@@ -1,30 +1,89 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void sieveSOD(vector<int> &sod){
-    for (int i=2; i<sod.size(); i++){
-        sod[i]+=i+1;
-        for (int j=i*i; j<sod.size(); j+=i) {
-            if (j==i*i) sod[j] += i;
-            else sod[j] += (i+j/i);
+// number divisor analysis 
+
+// NOD function calculation 
+// method 1 : from prime factorization 
+int nod_pfact(vector <pair <int, int>> &v) {
+    int res = 1;
+    for (auto [p, exp] : v) res *= (exp + 1);
+    return res;
+}
+
+// method 2 : looping until sqrt(n)
+int nod_sqrtn(long long int n) {
+    int res = 0;
+    for (long long int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            if (i * i == n) res++;
+            else res+=2;
         }
     }
-    sod[0] = 0;
-    sod[1] = 1;
-    return ;
+    return res;
+}
+
+// method 3 : sieve style
+// time complexity : O(nlogn)
+vector <int> nod;
+void init_nod(int n) {
+    nod.assign(n + 1, 1);
+    nod[0] = 0;
+    for (int i = 2; i <= n; ++i) {
+        for (int j = i; j <= n; j += i) {
+            nod[j]++;
+        }
+    }
 }
 
 
-int main(){
-    vector<int> sod(1e6+7,0);
-    sieveSOD(sod);
 
-    //Sum of divisors using sieve of Eratosthenes logic
-    //time complexity -> O(nlogn)
+
+// SOD function calculation 
+// method 1 : from prime factorization 
+long long int sod_pfact(vector <pair <int, int>> &v) {
+    long long int res = 1;
+    for (auto [p, exp] : v) {
+        long long int pn = 1;
+        for (int i = 0; i <= exp; i++) pn *= 1LL * p;
+        res *= (pn - 1) / (p - 1);
+    }
+    return res;
+}
+
+// method 2 : looping until sqrt(n)
+long long int sod_sqrtn(long long int n) {
+    long long int res = 0;
+    for (long long int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            if (i * i == n) res += i;
+            else res += i + n / i;
+        }
+    }
+    return res;
+}
+
+
+// method 3 : sieve style
+// time complexity : O(nlogn)
+vector <long long int> sod;
+void init_sod(int n) {
+    sod.resize(n + 1, 0);
+    for (long long int i = 1; i <= n; ++i) {
+        for (long long int j = i; j <= n; j += i) {
+            sod[j] += i;
+        }
+    }
+}
+
+
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
     
-    int x;
-    cin >> x;
-    for (int i=0; i<=x; i++) cout <<i<<" -> "<< sod[i] << "\n";
+
     return 0;
 }
