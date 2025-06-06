5475ab830fb49fccf1ee0cbc637aea0371058c46
Shuvo-06 <168964479+Shuvo-06@users.noreply.github.com>
2025-06-06 19:43:12 +0600

    Update modint.cpp

--- Data Structure /modint.cpp
+++ Data Structure /modint.cpp
@@ -4,30 +4,22 @@ using namespace std;
 template <int MOD>
 struct modint {
     int value;
+    modint(long long v = 0) : value((v % MOD + MOD) % MOD) {}
 
-    modint(long long v = 0) {
-        value = int(v % MOD);
-        if (value < 0) value += MOD;
-    }
+    modint& operator+=(const modint& o) { if ((value += o.value) >= MOD) value -= MOD; return *this; }
+    modint& operator-=(const modint& o) { if ((value -= o.value) < 0) value += MOD; return *this; }
+    modint& operator*=(const modint& o) { value = int(1LL * value * o.value % MOD); return *this; }
+    modint& operator/=(const modint& o) { return *this *= o.inv(); }
 
-    modint& operator+=(const modint& other) {
-        if ((value += other.value) >= MOD) value -= MOD;
-        return *this;
-    }
+    explicit operator bool() const { return value != 0; }
+    bool operator==(const modint& o) const { return value == o.value; }
+    bool operator!=(const modint& o) const { return value != o.value; }
 
-    modint& operator-=(const modint& other) {
-        if ((value -= other.value) < 0) value += MOD;
-        return *this;
-    }
-
-    modint& operator*=(const modint& other) {
-        value = int((1LL * value * other.value) % MOD);
-        return *this;
-    }
-
-    modint& operator/=(const modint& other) {
-        return *this *= other.inv();
-    }
+    modint operator-() const { return modint(-value); }
+    modint& operator++() { if (++value == MOD) value = 0; return *this; }
+    modint operator++(int) { modint t = *this; ++*this; return t; }
+    modint& operator--() { if (--value == -1) value = MOD - 1; return *this; }
+    modint operator--(int) { modint t = *this; --*this; return t; }
 
     friend modint operator+(modint a, const modint& b) { return a += b; }
     friend modint operator-(modint a, const modint& b) { return a -= b; }
@@ -35,22 +27,14 @@ struct modint {
     friend modint operator/(modint a, const modint& b) { return a /= b; }
 
     modint pow(long long p) const {
-        modint a = *this, result = 1;
-        while (p > 0) {
-            if (p & 1) result *= a;
-            a *= a;
-            p >>= 1;
-        }
-        return result;
+        modint a = *this, r = 1;
+        while (p) { if (p & 1) r *= a; a *= a; p >>= 1; }
+        return r;
     }
 
-    modint inv() const {
-        return this->pow(MOD - 2);
-    }
-
+    modint inv() const { return pow(MOD - 2); }
     int val() const { return value; }
 };
-
 using mint97 = modint<1000000007>;
 using mint99 = modint<998244353>;
 
