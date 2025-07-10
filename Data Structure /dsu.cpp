6c73315d0f2e995fc739ff0d927b7690a7bafbb8
Shuvo-06 <168964479+Shuvo-06@users.noreply.github.com>
2025-07-10 21:09:42 +0600

    Update dsu.cpp

--- Data Structure /dsu.cpp
+++ Data Structure /dsu.cpp
@@ -6,131 +6,101 @@ using namespace std;
 
     Features:
     - Path compression (in `find`) for nearly constant time queries.
-    - Union by size (in `union_sets`) to keep tree height small.
-    - Tracks parent nodes for reconstruction if needed.
+    - Union by size (in `unite`) to keep tree height small.
+    - Keep track of bipartite state
 
     Time Complexities:
     - find(v): O(α(n)), almost constant.
-    - union_sets(a, b), can_union_bipartite(u, v), union_bipartite(u, v) : O(α(n)), almost constant.
-    - get_parent(v): O(1), returns direct parent.
+    - unite(a, b) : O(α(n)), almost constant.
+    - unite_range(x, y) - O(n) in total
+    - same color(x, y) same_set(x, y) is_bipartite_after(x, y) : O(α(n)), almost constant.
+    - is_bipartite() : O(1)
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct {
+    int u, v, ind;
+    long long int w;
+} edge;
 
 class DSU {
-    vector<int> next;
-public:
-    vector<int> root, size, parent, color, depth;
     int n;
-    bool is_bipartite = true;
-    int num_components;
+    bool is_bpt = true;
+    vector <int> next;
 
-    DSU(int _n) : n(_n), num_components(_n) {
+public:
+    vector<int> size, color, root, rank; // color[v] = parity of distance from root (0 = same, 1 = different)
+
+    DSU(int _n) : n(_n) {
+        size.resize(n + 1, 1);
+        rank.resize(n + 1, 1);
+        color.resize(n + 1, 0);
+
         root.resize(n + 1);
-        size.resize(n + 1);
-        parent.resize(n + 1);
-        color.resize(n + 1);
         next.resize(n + 1);
-        depth.resize(n + 1, 0);
-        for (int i = 1; i <= n; ++i) {
-            root[i] = i;
-            size[i] = 1;
-            parent[i] = -1;
-            color[i] = 0;
-            next[i] = i + 1;
-        }
-        next[n] = n;
+        iota(root.begin(), root.end(), 0);
+        iota(next.begin(), next.end(), 0);
     }
 
     int find(int v) {
-        if (root[v] == v) return v;
+        if (v == root[v]) return v;
         int par = find(root[v]);
-        depth[v] += depth[root[v]];
-        return root[v] = par;
+        color[v] ^= color[root[v]]; // color[v] = (color[v] + color[root[v]]) % 2
+        root[v] = par;
+        return root[v];
     }
 
-    void union_sets(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b) {
-            if (size[a] < size[b]) swap(a, b);
-            root[b] = a;
-            size[a] += size[b];
-            parent[b] = a;
-            depth[b] = 1;
-            num_components--;
-        }
-    }
-
-    void union_bipartite(int a, int b) {
-        int ra = find(a);
-        int rb = find(b);
-        if (ra == rb) {
-            if ((color[a] ^ color[b]) != 1) is_bipartite = false;
-            return;
-        }
-
-        if (size[ra] < size[rb]) {
-            swap(ra, rb);
-            swap(a, b);
-        }
-
-        root[rb] = ra;
-        color[rb] = color[a] ^ color[b] ^ 1;
-        size[ra] += size[rb];
-        parent[rb] = ra;
-        depth[rb] = 1;
-        num_components--;
-    }
-
-    bool can_union_bipartite(int u, int v) {
+    bool unite(int u, int v) {
         int ru = find(u);
         int rv = find(v);
-        if (ru != rv) return true;
-        return (color[u] ^ color[v]) == 1;
+        if (ru == rv) {
+            if (color[u] == color[v]) is_bpt = false;
+            return false;
+        }
+
+        if (size[ru] < size[rv]) {
+            swap(u, v);
+            swap(ru, rv);
+        }
+
+        root[rv] = ru;
+        size[ru] += size[rv];
+        color[rv] = color[u] ^ color[v] ^ 1; // color[rv] = (color[u] + color[v] + 1) % 2
+        return true;
     }
 
-    void unite_range(int x, int y) {
-        int i = x;
-        while (i < y) {
-            int j = next[i];
-            if (j > y) break;
-            union_sets(i, j);
-            next[i] = next[j];
-            i = j;
+    int find_next(int i) { return (i == next[i] ? i : next[i] = find_next(next[i])); }
+
+    void unite_range(int l, int r) {
+        int x = find_next(l);
+        while (x < r) {
+            unite(x, x + 1);
+            next[x] = x + 1;
+            x = find_next(x);
         }
     }
 
-    int get_depth(int x) {
-        find(x);
-        return depth[x];
-    }
+    bool same_color(int u, int v) { return (find(u) == find(v) && color[u] == color[v]); }
+    bool same_set(int u, int v) { return find(u) == find(v); }
+    bool is_bipartite() const { return is_bpt; }
+    bool is_bipartite_after(int u, int v) { return (find(u) != find(v) || color[u] != color[v]);}
 };
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(NULL);
 
-    DSU dsu(5);
-    dsu.union_sets(1, 2);
-    dsu.union_sets(2, 4);
-    dsu.union_sets(5, 3);
+    #ifdef SUBLIME
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.out", "w", stdout);
+        freopen("error.txt", "w", stderr);
+    #endif
 
-    /*
-        Final DSU structure:
-
-        Component 1:  1
-                     / \
-                    2   4
-
-        Component 2: 3
-                     \
-                      5
-
-        Representative (leader) of:
-        - 1, 2, 4 → 1
-        - 3, 5 → 3
-    */
-
+    int n;
+    cin >> n;
+    DSU dsu(n);
+    
     return 0;
 }
