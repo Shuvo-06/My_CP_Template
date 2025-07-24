490c7cda4ab4a7b02b35fb2db0d862bfd5a29bbd
Shuvo-06 <168964479+Shuvo-06@users.noreply.github.com>
2025-07-25 01:45:48 +0600

    Update isPrimeOptimized.cpp

--- Number thoery/isPrimeOptimized.cpp
+++ Number thoery/isPrimeOptimized.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 
 bool isPrime(long long int n) {
-  if (n <= 1) return false;
-  if (n <= 3) return true;
-  if (n % 2 == 0 || n % 3 == 0) return false;
-  for (long long int i = 5; i * i <= n; i += 6) {
-    if (n % i == 0 || n % (i + 2) == 0) return false;
-  }
-  return true;
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (long long int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    }
+    return true;
 }
+
 
 //this is the traditional function
 /*
