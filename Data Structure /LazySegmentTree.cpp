5ad30a76231b82e0ad498aaecc70b3d1bdf36a82
Shuvo-06 <168964479+Shuvo-06@users.noreply.github.com>
2025-07-25 21:24:33 +0600

    Update LazySegmentTree.cpp

--- Data Structure /LazySegmentTree.cpp
+++ Data Structure /LazySegmentTree.cpp
@@ -2,111 +2,129 @@
 using namespace std;
 
 class LazySegmentTree {
-private:
+private: 
     int n;
-    vector<long long int> seg, lazy_add, lazy_set;
-    vector<bool> pending;
+    vector<long long> seg, lazy_add, lazy_set;
+    vector<bool> set_pending;
 
-    tuple <int, int, int> get_child(int idx, int lo, int hi) {
+    tuple<int, int, int> get_child(int idx, int lo, int hi) {
         int mid = lo + (hi - lo) / 2;
         return {mid, idx + 1, idx + 2 * (mid - lo + 1)};
     }
 
+    void build(vector<long long> &v, int idx, int lo, int hi) {
+        if (lo == hi) {
+            seg[idx] = v[lo]; // change here if needed
+            return;
+        }
+
+        auto [mid, lci, rci] = get_child(idx, lo, hi);
+        build(v, lci, lo, mid);
+        build(v, rci, mid + 1, hi);
+        seg[idx] = seg[lci] + seg[rci]; // change here if needed
+    }
+
     void push(int idx, int lo, int hi) {
-        if (pending[idx]) {
-            seg[idx] = (hi - lo + 1) * lazy_set[idx];
+        if (set_pending[idx]) {
+            seg[idx] = lazy_set[idx] * (hi - lo + 1LL); // set entire segment
             if (lo != hi) {
                 auto [mid, lci, rci] = get_child(idx, lo, hi);
-
                 lazy_set[lci] = lazy_set[rci] = lazy_set[idx];
-                pending[lci] = pending[rci] = true;
+                set_pending[lci] = set_pending[rci] = true;
                 lazy_add[lci] = lazy_add[rci] = 0;
             }
-            pending[idx] = false;
-            lazy_add[idx] = 0;
+            lazy_set[idx] = 0;
+            set_pending[idx] = false;
         }
 
-        if (lazy_add[idx] != 0) {
-            seg[idx] += (hi - lo + 1) * lazy_add[idx];
+        if (lazy_add[idx]) {
+            seg[idx] += lazy_add[idx] * (hi - lo + 1LL);
             if (lo != hi) {
                 auto [mid, lci, rci] = get_child(idx, lo, hi);
-
-                if (!pending[lci]) lazy_add[lci] += lazy_add[idx];
-                else lazy_set[lci] += lazy_add[idx];
-
-                if (!pending[rci]) lazy_add[rci] += lazy_add[idx];
-                else lazy_set[rci] += lazy_add[idx];
+                lazy_add[lci] += lazy_add[idx];
+                lazy_add[rci] += lazy_add[idx];
             }
             lazy_add[idx] = 0;
         }
     }
 
-    void build(int idx, vector<long long int> &v, int lo, int hi) {
-        if (lo == hi) {
-            seg[idx] = v[lo];
+    void add(int idx, int clo, int chi, int qlo, int qhi, long long int val) {
+        push(idx, clo, chi);
+        if (chi < qlo || clo > qhi) return;
+        if (qlo <= clo && chi <= qhi) {
+            lazy_add[idx] += val; 
+            push(idx, clo, chi);
             return;
         }
 
-        auto [mid, lci, rci] = get_child(idx, lo, hi);
-        build(lci, v, lo, mid);
-        build(rci, v, mid + 1, hi);
-        seg[idx] = seg[lci] + seg[rci];
+        auto [mid, lci, rci] = get_child(idx, clo, chi);
+        add(lci, clo, mid, qlo, qhi, val);
+        add(rci, mid + 1, chi, qlo, qhi, val); 
+        seg[idx] = seg[lci] + seg[rci]; // change here if needed
     }
 
-    void update_set(int idx, int lo, int hi, int clo, int chi, long long int val) {
-        push(idx, lo, hi);
-        if (chi < lo || hi < clo) return;
-        if (clo <= lo && hi <= chi) {
+    void set(int idx, int clo, int chi, int qlo, int qhi, long long int val) {
+        push(idx, clo, chi);
+        if (chi < qlo || clo > qhi) return;
+        if (qlo <= clo && chi <= qhi) {
             lazy_set[idx] = val;
-            pending[idx] = true;
+            set_pending[idx] = true;
             lazy_add[idx] = 0;
-            push(idx, lo, hi);
+            push(idx, clo, chi);
             return;
         }
 
-        auto [mid, lci, rci] = get_child(idx, lo, hi);
-        update_set(lci, lo, mid, clo, chi, val);
-        update_set(rci, mid + 1, hi, clo, chi, val);
-        seg[idx] = seg[lci] + seg[rci];
+        auto [mid, lci, rci] = get_child(idx, clo, chi);
+        set(lci, clo, mid, qlo, qhi, val);
+        set(rci, mid + 1, chi, qlo, qhi, val);
+        seg[idx] = seg[lci] + seg[rci]; // change here if needed
     }
 
-    void update_add(int idx, int lo, int hi, int clo, int chi, long long int val) {
-        push(idx, lo, hi);
-        if (chi < lo || hi < clo) return;
-        if (clo <= lo && hi <= chi) {
-            lazy_add[idx] += val;
-            push(idx, lo, hi);
-            return;
-        }
+    long long int query(int idx, int clo, int chi, int qlo, int qhi) {
+        push(idx, clo, chi);
+        if (chi < qlo || clo > qhi) return 0LL; // change here if needed
+        if (qlo <= clo && chi <= qhi) return seg[idx];
 
-        auto [mid, lci, rci] = get_child(idx, lo, hi);
-        update_add(lci, lo, mid, clo, chi, val);
-        update_add(rci, mid + 1, hi, clo, chi, val);
-        seg[idx] = seg[lci] + seg[rci];
+        auto [mid, lci, rci] = get_child(idx, clo, chi);
+        auto q1 = query(lci, clo, mid, qlo, qhi);
+        auto q2 = query(rci, mid + 1, chi, qlo, qhi);
+        return q1 + q2; // change here if needed
     }
 
-    long long int query(int idx, int lo, int hi, int clo, int chi) {
-        push(idx, lo, hi);
-        if (chi < lo || hi < clo) return 0LL;
-        if (clo <= lo && hi <= chi) return seg[idx];
-
-        auto [mid, lci, rci] = get_child(idx, lo, hi);
-        return query(lci, lo, mid, clo, chi) + query(rci, mid + 1, hi, clo, chi);
-    }
-
-public:
-    LazySegmentTree(int _n) : n(_n) {
-        seg.assign(2 * n, 0);
+public: 
+    void resize(int _n) { 
+        n = _n; 
+        seg.assign(2 * n, 0); 
         lazy_add.assign(2 * n, 0);
         lazy_set.assign(2 * n, 0);
-        pending.assign(2 * n, false);
+        set_pending.assign(2 * n, false);
     }
 
-    void build(vector<long long int> &v) { build(0, v, 0, n - 1); }
-    void set(int l, int r, long long int val) { update_set(0, 0, n - 1, l, r, val); }
-    void add(int l, int r, long long int val) { update_add(0, 0, n - 1, l, r, val); }
-    long long query(int l, int r) { return query(0, 0, n - 1, l, r); }
+    void build(vector<long long> &v) { 
+        n = v.size();
+        seg.assign(2 * n, 0); 
+        lazy_add.assign(2 * n, 0);
+        lazy_set.assign(2 * n, 0);
+        set_pending.assign(2 * n, false);
+        build(v, 0, 0, n - 1); 
+    }
+
+    void add(int lo, int hi, long long val) { add(0, 0, n - 1, lo, hi, val); }
+    void add(int pos, long long val) { add(0, 0, n - 1, pos, pos, val); }
+    void set(int lo, int hi, long long val) { set(0, 0, n - 1, lo, hi, val); }
+    void set(int pos, long long val) { set(0, 0, n - 1, pos, pos, val); }
+    long long query(int lo, int hi) { return query(0, 0, n - 1, lo, hi); }
+    long long query(int pos) { return query(0, 0, n - 1, pos, pos); }
 };
+
+/*
+Lazy Segment tree
+used for RU-PQ and RU-RQ
+you can use it when operations are associative and distributive
+Time complexity : 
+    build : O(nlogn)
+    rest of the operations are O(log N)
+*/
 
 int main() {
     ios::sync_with_stdio(0);
@@ -116,10 +134,10 @@ int main() {
     cin >> n >> q;
     vector <long long int> v(n);
     for (auto &x : v) cin >> x;
-
-    LazySegmentTree st(n);
+ 
+    LazySegmentTree st;
     st.build(v);
-
+ 
     while (q--) {
         int op, l, r;
         cin >> op >> l >> r;
@@ -135,4 +153,5 @@ int main() {
         }
         else cout << st.query(l - 1, r - 1) << "\n";
     }
+    return 0;
 }
